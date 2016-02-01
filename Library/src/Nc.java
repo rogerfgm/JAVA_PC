67cb9f8d90fa04cd3d9a25ad3383df3f96f613a6
daisuke <dafukuda@gmail.com>
2016-02-01 10:18:28 +0900

    c

--- Library/src/Nc.java
+++ Library/src/Nc.java
@@ -2,6 +2,35 @@ import java.math.BigInteger;
 
 
 public class Nc {
+	
+	// nCr mod p
+	public long C(int n, int r, int p)
+	{
+		long ret = 1;
+		while(true){
+			if(r == 0)break;
+			int N = n % p;
+			int R = r % p;
+			if(N < R)return 0;
+	 
+			for(int i = 0;i < R;i++){
+				ret = ret * (N-i) % p;
+			}
+			long imul = 1;
+			for(int i = 0;i < R;i++){
+				imul = imul * (i+1) % p;
+			}
+			ret = ret * mod_inverse(imul, p) % p;
+			n /= p; r /= p;
+		}
+		return ret;
+	}
+	
+	long mod_inverse(long a, long b){
+		BigInteger x = BigInteger.valueOf(a);
+		BigInteger y = BigInteger.valueOf(b);
+		return x.modInverse(y).longValue();
+	}
 
 	static BigInteger ncb(final int n, int c) {
 		if(n < c){
